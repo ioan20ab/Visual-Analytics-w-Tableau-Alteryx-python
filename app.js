0efa24d6502671ec80dd71c29d7a576c882ff2dd
ioan20ab <71883773+ioan20ab@users.noreply.github.com>
2022-03-31 23:23:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const url = "https://public.tableau.com/views/TelemetryF1_16487609759020/Dashboa
 const vizContainer = document.getElementById('vizContainer');
 const options = {
     hideTabs: true,
-    height: 1280,
-    width: 3000,
+    height: 1000,
+    width: 1200,
     onFirstInteraction: function() {
         workbook = viz.getWorkbook();
         activeSheet = workbook.getActiveSheet();
